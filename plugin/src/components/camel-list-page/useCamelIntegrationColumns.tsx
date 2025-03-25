6e14633f2c4efaa13491222d79b3f83c44e7ceee
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-03-25 18:38:11 +0100

    Add sorting on status column

--- plugin/src/components/camel-list-page/useCamelIntegrationColumns.tsx
+++ plugin/src/components/camel-list-page/useCamelIntegrationColumns.tsx
@@ -1,6 +1,18 @@
 import { K8sResourceKind, TableColumn } from '@openshift-console/dynamic-plugin-sdk';
 import { sortable } from '@patternfly/react-table';
 import { useTranslation } from 'react-i18next';
+import { CamelIntegrationStatusValue } from './CamelIntegrationStatus';
+
+export const sortResourceByStatus =
+  (direction: string) => (a: K8sResourceKind, b: K8sResourceKind) => {
+    const { first, second } =
+      direction === 'asc' ? { first: a, second: b } : { first: b, second: a };
+
+    const firstValue = CamelIntegrationStatusValue(first);
+    const secondValue = CamelIntegrationStatusValue(second);
+
+    return firstValue?.localeCompare(secondValue);
+  };
 
 const useCamelIntegrationColumns = (namespace): TableColumn<K8sResourceKind>[] => {
   const { t } = useTranslation('plugin__camel-openshift-console-plugin');
@@ -30,8 +42,8 @@ const useCamelIntegrationColumns = (namespace): TableColumn<K8sResourceKind>[] =
     {
       title: t('Status'),
       id: 'status',
-      sort: 'status.phase',
-      transforms: [],
+      sort: (data, direction) => data?.sort(sortResourceByStatus(direction)),
+      transforms: [sortable],
     },
     {
       title: t('Camel'),
