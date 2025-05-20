3880d4c618db5fbc30c7138af3b6c8a8b388333c
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-05-20 10:09:08 +0200

    WIP

---
+++ src/components/camel-app-metrics/CamelAppMetrics.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { CamelAppKind } from '../../types';
+
+type CamelAppMetricsProps = {
+  obj: CamelAppKind;
+};
+
+const CamelAppMetrics: React.FC<CamelAppMetricsProps> = ({ obj: camelInt }) => {
+  
+
+  return (
+    <></>
+  );
+};
+
+export default CamelAppMetrics;
