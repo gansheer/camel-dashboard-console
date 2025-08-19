0965023fe4fa5de493b5a8893f1b6f71dc2eb428
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-08-19 15:55:23 +0200

    fix: improve hawtio link visual

---
+++ src/components/camel-app-resources/HawtioIcon.tsx
@@ -0,0 +1,6 @@
+import HawtioImage from '@images/hawtio.svg';
+import React, { ComponentType } from 'react';
+
+export const HawtioIcon: ComponentType = () => {
+  return <img src={HawtioImage} alt="Hawtio" width="50px" height="50px" />;
+};
